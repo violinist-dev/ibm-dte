77c008714ad074ee8adaf67a16b031403168bca6
Sam Richard <sam@snug.ug>
2018-06-29 15:50:58 -0400

    :crystal_ball: FINE! Show me vendor

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 git checkout -b deploy`date +'%Y-%m-%d-%H-%M-%S-%Z'`
 git status
 
-git add .
-git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
+ls vendor
+
+# git add .
+# git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
 lando ssh -c "git push pantheon master"
