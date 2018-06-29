8f78bf0f09aeb46d1bba65b2323a1c02e0c061ed
Sam Richard <sam@snug.ug>
2018-06-29 14:26:42 -0400

    :crystal_ball: Maybe checking out master will help?

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 # Rename origin to upstream, set
 git remote rename origin upstream
 git remote add origin $PANTHEON_GIT
+git checkout -b master
 git branch --set-upstream-to origin
 
 lando push --files=none --database=none -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
