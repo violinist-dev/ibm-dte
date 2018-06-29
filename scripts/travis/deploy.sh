d8109049b987fb51d84d254627d3cbd51fba5377
Sam Richard <sam@snug.ug>
2018-06-29 11:43:17 -0400

    :crystal_ball: Pipe output and rearrange git

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,4 +2,10 @@
 
 set -eo pipefail
 
+git remote rename origin upstream
+git remote add origin $PANTHEON_GIT
+
 lando push --files=none --database=none -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
+
+git remote remove origin
+git remote rename upstream origin
