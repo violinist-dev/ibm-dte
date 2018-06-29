cbebc0a2a726af7fa44e73e2afbeaab708bfa0cb
Sam Richard <sam@snug.ug>
2018-06-29 15:59:24 -0400

    :crystal_ball: How about I CHECK OUT MASTER

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,11 +2,8 @@
 
 set -eo pipefail
 
-git checkout -b deploy`date +'%Y-%m-%d-%H-%M-%S-%Z'`
-git status
+git checkout master
 
-ls vendor
-
-# git add .
-# git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
+git add .
+git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
 lando ssh -c "git push pantheon master"
