29f5e043ff3bf603e71c230d0f7f6f246b15d708
Sam Richard <sam@snug.ug>
2018-06-29 15:16:40 -0400

    :crystal_ball: If there's nothing to commit, maybe just push?

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,17 +2,13 @@
 
 set -eo pipefail
 
-# Did Skip Cleanup Not Work?
-lando composer install
-lando composer prepare-for-pantheon
-
 # Rename origin to upstream, set
 git remote rename origin upstream
 git remote add origin $PANTHEON_GIT
 git remote -v
 
-git add .
-git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
+# git add .
+# git commit -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
 lando ssh -c "git -C push origin master"
 
 git remote remove origin
