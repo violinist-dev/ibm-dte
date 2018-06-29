6dbad03fe2aa2ab30442a399fd1ee3ad833cf8be
Sam Richard <sam@snug.ug>
2018-06-29 14:08:17 -0400

    :crystal_ball: Update to supported --set-upstream-to

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -5,10 +5,10 @@ set -eo pipefail
 # Rename origin to upstream, set
 git remote rename origin upstream
 git remote add origin $PANTHEON_GIT
-git branch --set-upstream origin
+git branch --set-upstream-to origin
 
 lando push --files=none --database=none -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
 
 git remote remove origin
 git remote rename upstream origin
-git branch --set-upstream origin
+git branch --set-upstream-to origin
