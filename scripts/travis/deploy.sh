3d54d4e0fd0ce485e3bf2ef7b4866475a942a045
Sam Richard <sam@snug.ug>
2018-06-29 14:00:56 -0400

    :crystal_ball: Set upstream branch before push

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,10 +2,13 @@
 
 set -eo pipefail
 
+# Rename origin to upstream, set
 git remote rename origin upstream
 git remote add origin $PANTHEON_GIT
+git branch --set-upstream origin
 
 lando push --files=none --database=none -m "Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`"
 
 git remote remove origin
 git remote rename upstream origin
+git branch --set-upstream origin
