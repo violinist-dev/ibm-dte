cdc9258335963272639ea32ae951a0541fea99f3
Sam Richard <sam@snug.ug>
2018-07-02 16:33:50 -0500

    :crystal_ball: Force Pushing like a crazy person!

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -8,8 +8,6 @@ lando ssh -c "git fetch -q --all"
 lando ssh -c "git checkout master"
 lando ssh -c "git branch --set-upstream-to pantheon/master"
 
-lando ssh -c "git pull pantheon master 1>/dev/null 2>&2"
-
 lando ssh -c "git add . 1>/dev/null 2>&2"
 lando ssh -c "git commit -m \"Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`\" 1>/dev/null 2>&2"
-lando ssh -c "git push pantheon master 1>/dev/null 2>&2"
+lando ssh -c "git push -f pantheon master 1>/dev/null 2>&2"
