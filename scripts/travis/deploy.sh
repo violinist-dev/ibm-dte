04f253f17bf5330f9769947e25e95fb78a55a0b6
Sam Richard <sam@snug.ug>
2018-07-02 16:16:41 -0500

    :crystal_ball: Pull before pushing

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -4,9 +4,11 @@ set -eo pipefail
 
 lando ssh -c "git remote -v"
 
-lando ssh -c "git fetch --all"
+lando ssh -c "git fetch -q --all"
 lando ssh -c "git checkout master"
 lando ssh -c "git branch --set-upstream-to pantheon/master"
+
+lando ssh -c "git pull pantheon master 1>/dev/null 2>&2"
 
 lando ssh -c "git add . 1>/dev/null 2>&2"
 lando ssh -c "git commit -m \"Deploy from Travis - `date +'%Y-%m-%d %H:%M:%S %Z'`\" 1>/dev/null 2>&2"
