c08f8a485eeedad6ca3f54db8531cbc9dac5e042
Dallas Hudgens <dallashudgens@us.ibm.com@dallass-mbp.austin.ibm.com>
2018-10-02 16:17:00 -0500

    adjust selector for adding support icon

--- web/modules/custom/gm_addons/js/menu-target.js
+++ web/modules/custom/gm_addons/js/menu-target.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
     //find the cms support link
     //if it's there, add target attribute 
     //set attribute to blank
-    const supportLink = document.querySelector('#toolbar-item-workbench-tray .menu-item a[href="https://ibm-dte.slack.com/app_redirect?channel=cms"] ');
+    const supportLink = document.querySelector('#toolbar-item-workbench-tray .menu-item a[href="https://ibm-dte.slack.com/app_redirect?channel=cms"]');
   
     if (supportLink) {
       supportLink.setAttribute('target', '_blank');
